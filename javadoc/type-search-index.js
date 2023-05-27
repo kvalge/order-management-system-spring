ee35a411dd566f6a71f1cad8c9ae68836ac29aa6
Katrin Valge <katrin.valge@mail.ee>
2023-05-27 15:09:05 +0300

    Created javadoc.

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.ordermanagementsystemspring","l":"OrderManagementSystemSpringApplication"},{"p":"com.example.ordermanagementsystemspring","l":"OrderManagementSystemSpringApplicationTests"}];updateSearchResults();
