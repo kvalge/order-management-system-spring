d5a6701946fd10f4d917335f1be28c82e8298205
Katrin Valge <katrin.valge@mail.ee>
2023-05-27 14:58:07 +0300

    Initial commit.

---
+++ src/test/java/com/example/ordermanagementsystemspring/OrderManagementSystemSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordermanagementsystemspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementSystemSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
