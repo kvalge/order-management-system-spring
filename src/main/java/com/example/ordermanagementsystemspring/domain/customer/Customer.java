a49560c6262e17921bb1b97683f03e25f5738acb
Katrin Valge <katrin.valge@mail.ee>
2023-05-27 15:32:45 +0300

    Created customer entity class.

---
+++ src/main/java/com/example/ordermanagementsystemspring/domain/customer/Customer.java
@@ -0,0 +1,27 @@
+package com.example.ordermanagementsystemspring.domain.customer;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(length = 50, unique = true, nullable = false)
+    private String registrationCode;
+
+    @Column(length = 250, unique = true, nullable = false)
+    private String full_name;
+
+    @Column(length = 50, unique = true, nullable = false)
+    private String email;
+
+    @Column(length = 50, unique = true, nullable = false)
+    private String telephone;
+}
