5a1e6d196540c188afc110201d1cd5c4d86d1817
Katrin Valge <katrin.valge@mail.ee>
2023-05-27 16:30:03 +0300

    Created createCustomer methods.

--- src/main/java/com/example/ordermanagementsystemspring/domain/customer/CustomerController.java
+++ src/main/java/com/example/ordermanagementsystemspring/domain/customer/CustomerController.java
@@ -1,9 +1,26 @@
 package com.example.ordermanagementsystemspring.domain.customer;
 
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class CustomerController {
+
+    @Resource
+    private CustomerService customerService;
+
+    @PostMapping(value = "/customer", produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<CustomerDto> createCustomer(@RequestBody CustomerDto customerDto) {
+        log.debug("REST request to createCustomer Customer");
+        return ResponseEntity
+                .ok()
+                .body(customerService.createCustomer(customerDto));
+    }
 }
--- src/main/java/com/example/ordermanagementsystemspring/domain/customer/CustomerService.java
+++ src/main/java/com/example/ordermanagementsystemspring/domain/customer/CustomerService.java
@@ -1,7 +1,27 @@
 package com.example.ordermanagementsystemspring.domain.customer;
 
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
+@Slf4j
 public class CustomerService {
+
+    @Resource
+    private CustomerMapper customerMapper;
+
+    @Resource
+    private CustomerRepository customerRepository;
+
+    public CustomerDto createCustomer(CustomerDto customerDto) {
+        log.debug("Request to createCustomer Customer : {}", customerDto);
+
+        Customer customer = customerMapper.toEntity(customerDto);
+        customer = customerRepository.save(customer);
+
+        return customerMapper.toDto(customer);
+    }
 }
