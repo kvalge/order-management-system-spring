b552bf8fe40bba9dfdc9edc6482698620e6a58e9
Katrin Valge <katrin.valge@mail.ee>
2023-05-28 14:23:02 +0300

    Created product exception.

---
+++ src/main/java/com/example/ordermanagementsystemspring/domain/exception/ProductException.java
@@ -0,0 +1,13 @@
+package com.example.ordermanagementsystemspring.domain.exception;
+
+public class ProductException extends AppException {
+
+    public ProductException(String message, int code) {
+        super(message);
+        setCode(code);
+    }
+
+    public ProductException(Exception e) {
+        super(e);
+    }
+}
