0bab178e73f98c2e17b832cfc52609e53d8da299
Katrin Valge <katrin.valge@mail.ee>
2023-05-28 14:09:54 +0300

    Updated customer model.

--- src/main/java/com/example/ordermanagementsystemspring/domain/model/Customer.java
+++ src/main/java/com/example/ordermanagementsystemspring/domain/model/Customer.java
@@ -9,14 +9,13 @@ import lombok.Data;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "identityGenerator")
     private Long id;
 
     @Column(length = 50, unique = true, nullable = false)
     private String registrationCode;
 
-    @Column(length = 250, unique = true, nullable = false)
+    @Column(length = 250, nullable = false)
     private String full_name;
 
     @Column(length = 50, unique = true, nullable = false)
