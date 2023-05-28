3f096a0ea00a625b40a2c71470e0ff8c2e928373
Katrin Valge <katrin.valge@mail.ee>
2023-05-28 16:22:18 +0300

    Updated methods.

--- src/main/java/com/example/ordermanagementsystemspring/domain/exception/ProductException.java
+++ src/main/java/com/example/ordermanagementsystemspring/domain/exception/ProductException.java
@@ -1,6 +1,9 @@
 package com.example.ordermanagementsystemspring.domain.exception;
 
 public class ProductException extends AppException {
+    public ProductException(String message) {
+        super(message);
+    }
 
     public ProductException(String message, int code) {
         super(message);
