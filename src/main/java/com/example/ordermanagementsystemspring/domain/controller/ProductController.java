368bfa0597ec58f3339d90ef49fc07aabf87a682
Katrin Valge <katrin.valge@mail.ee>
2023-05-28 14:52:29 +0300

    Created createProduct methods.

--- src/main/java/com/example/ordermanagementsystemspring/domain/controller/ProductController.java
+++ src/main/java/com/example/ordermanagementsystemspring/domain/controller/ProductController.java
@@ -1,6 +1,12 @@
 package com.example.ordermanagementsystemspring.domain.controller;
 
+import com.example.ordermanagementsystemspring.domain.service.ProductService;
+import com.example.ordermanagementsystemspring.domain.service.dto.ProductDto;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @Slf4j
 public class ProductController {
+
+    @Resource
+    private ProductService productService;
+
+    @PostMapping(value = "/product", produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto) {
+        log.info("REST request to createProduct Product");
+        return ResponseEntity
+                .ok()
+                .body(productService.save(productDto));
+    }
 }
--- src/main/java/com/example/ordermanagementsystemspring/domain/service/ProductService.java
+++ src/main/java/com/example/ordermanagementsystemspring/domain/service/ProductService.java
@@ -1,7 +1,39 @@
 package com.example.ordermanagementsystemspring.domain.service;
 
+import com.example.ordermanagementsystemspring.domain.model.Product;
+import com.example.ordermanagementsystemspring.domain.repository.ProductRepository;
+import com.example.ordermanagementsystemspring.domain.service.dto.ProductDto;
+import com.example.ordermanagementsystemspring.domain.service.mapper.ProductMapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
+@Transactional
+@Slf4j
 public class ProductService {
+
+    @Resource
+    private ProductMapper productMapper;
+
+    @Resource
+    private ProductRepository productRepository;
+
+    /**
+     * Product inserted price is given in cents, returned price is calculated to euros.
+     */
+    public ProductDto save(ProductDto productDto) {
+        log.info("Request to save Product : {}", productDto);
+
+        Product product = productMapper.toEntity(productDto);
+        product.setSkuCode(UUID.randomUUID().toString());
+        product = productRepository.save(product);
+        float price = (float) Math.round((productDto.getUnitPrice() / 100) * 100) / 100;
+        product.setUnitPrice (price);
+
+        return productMapper.toDto(product);
+    }
 }
