fef337a0b306273bf3ade378d2647903755e6ea1
Katrin Valge <katrin.valge@mail.ee>
2023-05-29 18:48:58 +0300

    Added media type.

--- src/main/java/com/example/ordermanagementsystemspring/domain/service/OrderService.java
+++ src/main/java/com/example/ordermanagementsystemspring/domain/service/OrderService.java
@@ -11,7 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,13 +36,26 @@ public class OrderService {
         Order order = orderMapper.toEntity(orderDto);
         Optional<Customer> customer = customerRepository.findById(orderDto.getCustomerId());
         order.setCustomer(customer.get());
-        Date date = new Date();
-        order.setSubmissionDate(date);
+        order.setSubmissionDate(LocalDate.now());
+
         orderRepository.save(order);
 
         OrderDto dto = orderMapper.toDto(order);
         dto.setCustomerId(customer.get().getId());
 
         return dto;
+    }
+
+    public List<OrderDto> findByDate(LocalDate date) {
+        List<Order> orders = orderRepository.findAllBySubmissionDate(date);
+        List<OrderDto> orderDtos = orderMapper.toDtoList(orders);
+        for (Order order : orders) {
+            for (OrderDto orderDto : orderDtos) {
+                if (Objects.equals(order.getId(), orderDto.getId())) {
+                    orderDto.setCustomerId(order.getCustomer().getId());
+                }
+            }
+        }
+        return orderDtos;
     }
 }
