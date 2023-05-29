072b8178b43e2bf324a8632dd5fe5f6ceff661fa
Katrin Valge <katrin.valge@mail.ee>
2023-05-29 12:48:23 +0300

    OrderController: created createOrder method. OrderService: created save method.

--- src/main/java/com/example/ordermanagementsystemspring/domain/controller/OrderController.java
+++ src/main/java/com/example/ordermanagementsystemspring/domain/controller/OrderController.java
@@ -1,6 +1,13 @@
 package com.example.ordermanagementsystemspring.domain.controller;
 
+import com.example.ordermanagementsystemspring.domain.service.OrderService;
+import com.example.ordermanagementsystemspring.domain.service.dto.OrderDto;
+import com.example.ordermanagementsystemspring.domain.service.dto.OrderLineDto;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @Slf4j
 public class OrderController {
+
+    @Resource
+    private OrderService orderService;
+
+    @PostMapping(value = "/order", produces = {"application/json"}, consumes = {"application/json"})
+    public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto orderDto) {
+        log.info("REST request to create Order");
+
+        return ResponseEntity
+                .ok()
+                .body(orderService.save(orderDto));
+    }
 }
---
+++ src/main/java/com/example/ordermanagementsystemspring/domain/service/OrderService.java
@@ -0,0 +1,43 @@
+package com.example.ordermanagementsystemspring.domain.service;
+
+import com.example.ordermanagementsystemspring.domain.model.Customer;
+import com.example.ordermanagementsystemspring.domain.model.Order;
+import com.example.ordermanagementsystemspring.domain.repository.CustomerRepository;
+import com.example.ordermanagementsystemspring.domain.repository.OrderRepository;
+import com.example.ordermanagementsystemspring.domain.service.dto.OrderDto;
+import com.example.ordermanagementsystemspring.domain.service.mapper.OrderMapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@Slf4j
+public class OrderService {
+
+    @Resource
+    private OrderMapper orderMapper;
+
+    @Resource
+    private OrderRepository orderRepository;
+
+    @Resource
+    private CustomerRepository customerRepository;
+
+    public OrderDto save(OrderDto orderDto) {
+        log.info("Request to save Order : {}", orderDto);
+
+        Order order = orderMapper.toEntity(orderDto);
+        Optional<Customer> customer = customerRepository.findById(orderDto.getCustomerId());
+        order.setCustomer(customer.get());
+        orderRepository.save(order);
+
+        OrderDto dto = orderMapper.toDto(order);
+        dto.setCustomerId(customer.get().getId());
+
+        return dto;
+    }
+}
