7f274300f00b872d86fb521c500d6b4d4ca0f567
Katrin Valge <katrin.valge@mail.ee>
2023-05-30 17:58:49 +0300

    Created datafiles.

---
+++ src/main/resources/data/customer.sql
@@ -0,0 +1,11 @@
+INSERT INTO customer (id, registration_code, full_name, email, telephone)
+VALUES (DEFAULT, '0000001', 'Kati Karu', 'karukati@mail-com', '51610000');
+
+INSERT INTO customer (id, registration_code, full_name, email, telephone)
+VALUES (DEFAULT, '0000002', 'Mati Karu', 'karumati@mail-com', '51610001');
+
+INSERT INTO customer (id, registration_code, full_name, email, telephone)
+VALUES (DEFAULT, '0000003', 'Rein Rebase', 'rebaserein@mail-com', '51610002');
+
+INSERT INTO customer (id, registration_code, full_name, email, telephone)
+VALUES (DEFAULT, '0000004', 'Juta Jänku', 'jankujuta@mail-com', '51610003');
---
+++ src/main/resources/data/product.sql
@@ -0,0 +1,8 @@
+INSERT INTO product (id, name, sku_code, unit_price)
+VALUES (DEFAULT, 'Chess', 100000001, 3999);
+
+INSERT INTO product (id, name, sku_code, unit_price)
+VALUES (DEFAULT, 'Chequers', 100000002, 1999);
+
+INSERT INTO product (id, name, sku_code, unit_price)
+VALUES (DEFAULT, 'Domino', 100000002, 999);
