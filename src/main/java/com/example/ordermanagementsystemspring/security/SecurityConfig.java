413f79e2868ec4d32dd9c3fa0feaa55b1f296791
Katrin Valge <katrin.valge@mail.ee>
2023-06-29 23:40:08 +0300

    Updated basic authentication.

--- src/main/java/com/example/ordermanagementsystemspring/security/SecurityConfig.java
+++ src/main/java/com/example/ordermanagementsystemspring/security/SecurityConfig.java
@@ -2,13 +2,13 @@ package com.example.ordermanagementsystemspring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -20,16 +20,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authCustomizer -> authCustomizer
                 .requestMatchers("/api/**").permitAll());
         http.csrf(csrf -> csrf.disable());
+        http.httpBasic(withDefaults());
         return http.build();
     }
 }
